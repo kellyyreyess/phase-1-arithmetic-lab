ac4a41c777a1e757e299e17e1aabd11b43edfc29
Kelly Reyes <kellyreyes219@gmail.com>
2023-10-11 20:28:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let apples = 1
+let berries = 5
+let random = apples + berries
+
+let num3 = 8
+let num4 = 2
+let mod = num3 / num4 
+
+let num5 = 19
+let num6 = 1
+let max = num5 + num6
